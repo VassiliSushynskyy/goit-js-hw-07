61eeb1a91133808dfe8d8f001adf74cabf00a0b3
VasylSushynskyy <92335667+VassiliSushynskyy@users.noreply.github.com>
2022-05-04 23:25:03 +0200

    7.1 finish

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,7 +1,35 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 const galleryEl = document.querySelector('.gallery');
+ 
+const creatDivElementmarkup = galleryItems.map(({preview, original, description}) => {
+   return `<div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div >
+    `
+}).join("");
+  
+galleryEl.insertAdjacentHTML('afterbegin', creatDivElementmarkup);
+galleryEl.addEventListener('click', modalPreviev)
 
+  
+function modalPreviev(ev) {
+    ev.preventDefault()
+    if (ev.target.nodeName !== "IMG")  
+    { return }
+    const url = ev.target.dataset.source;
+    let instance = basicLightbox.create(`
+    <img src="${url}">
+  `);
+    instance.show()
+};
 
 
 
